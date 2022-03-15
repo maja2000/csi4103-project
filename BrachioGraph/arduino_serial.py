fc5ec1a995b8284d323314948d30d8b4f127a225
jward090 <nailuj@hotmail.ca>
2022-03-15 15:44:29 -0400

    working on the serial from the pi side

--- BrachioGraph/arduino_serial.py
+++ BrachioGraph/arduino_serial.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 import serial
 import time
-if __name__ == '__main__':
-    #Need to go through the JSON file and send the lines to the Arduino
 
+def txt_reader(txtName):
+    realTxtName ='BrachioGraph/images' + txtName
+    with open(txtName) as f:
+        data = f.read()
+    data += "/n"
+    return data
+
+if __name__ == '__main__':
+#if False:
+    #Need to go through the plaintext_coordinates.txt file and send the coordinates to the Arduino
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
+    txt_data = txt_reader("/home/pi/csi4103-project/BrachioGraph/images/plaintext_coordinates.txt").encode('utf-8')
+    #print(txt_data)
+    
+    ser.write(txt_data)
     while True:
-        ser.write(b"Hello from Raspberry Pi!\n")
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
-        time.sleep(1)
+      line = ser.readline().decode('utf-8').rstrip()
+      print(line)
+      time.sleep(1)
+
+#txt_reader("/home/pi/csi4103-project/BrachioGraph/images/plaintext_coordinates.txt")
