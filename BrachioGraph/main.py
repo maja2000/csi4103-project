1b0b7cf6bea40d5d9a2a31ee43cb87eec36c9e87
jward090 <nailuj@hotmail.ca>
2022-03-20 17:31:32 -0400

    created a JSON of the world

--- BrachioGraph/main.py
+++ BrachioGraph/main.py
@@ -4,7 +4,7 @@ from images import json_reader
 from flatten_array import array_to_2d
 import numpy
 
-image_to_json("africa.jpg", draw_contours=2, draw_hatch=16)
+image_to_json("world.jpg", draw_contours=2, draw_hatch=16)
 #This code finds an image with the indicated name in the 'images' directory and:
 #    -draws the contours and hatch lines
 #    -creates a JSON file with the same name
@@ -14,7 +14,7 @@ image_to_json("africa.jpg", draw_contours=2, draw_hatch=16)
 #    countours = 2 (between 0.5 and 4)
 #    draw_hatch = 16 (between 8 and 16)
 
-json_data = json_reader.read_json("images/africa.jpg.json")
+json_data = json_reader.read_json("images/world.jpg.json")
 array_2d = array_to_2d(json_data)
 json_reader.write_to_text(array_2d)
 
