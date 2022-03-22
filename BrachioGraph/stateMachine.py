3761fe5efdcaccd846952c0713785e26c1f2d201
Christopher DeLongchamp <52896126+CDelongchamp@users.noreply.github.com>
2022-03-22 12:23:55 -0400

    Update stateMachine.py
    Updated formatting

--- BrachioGraph/stateMachine.py
+++ BrachioGraph/stateMachine.py
@@ -3,22 +3,19 @@ from transitions import Machine
 
 
 class PenPlotter(object):
-
-
-    states = ['IDLE', 
-        'GETTING_USER_INPUT', 
-        'CONVERTING_JPG_TO_JSON', 
-        'FORMATTING_JSON',
-        'BAD_INPUT',
-        'SENDING_INSTRUCTIONS_TO_ARDUINO',
-        'AWAIT_BEGIN',
-        'RECEIVING_FROM_ARDUINO'
-        'E_STOP'
-        'DEFAULT'
-        ]
+    states = ['IDLE',
+              'GETTING_USER_INPUT',
+              'CONVERTING_JPG_TO_JSON',
+              'FORMATTING_JSON',
+              'BAD_INPUT',
+              'SENDING_INSTRUCTIONS_TO_ARDUINO',
+              'AWAIT_BEGIN',
+              'RECEIVING_FROM_ARDUINO'
+              'E_STOP'
+              'DEFAULT'
+              ]
 
     def __init__(self, plotterState):
-
         # Initialize in the Idle state.
         self.machine = Machine(model=self, states=PenPlotter.states, initial='IDLE')
 
@@ -27,13 +24,14 @@ class PenPlotter(object):
         self.machine.add_transition(trigger='user_input_received', source='IDLE', dest='GETTING_USER_INPUT')
 
         # When the machine has received all it needs from User input, it can then proceed to the next stage.
-        self.machine.add_transition(trigger='input_finalized', source='GETTING_USER_INPUT', dest='CONVERTING_JPG_TO_JSON')
+        self.machine.add_transition(trigger='input_finalized', source='GETTING_USER_INPUT',
+                                    dest='CONVERTING_JPG_TO_JSON')
 
-        # ERROR: When the file selected throws an error, we go to the BAD_INPUT.
+        # ERROR: When the file selected throws an error, we go to the BAD_INPUT state.
         self.machine.add_transition(trigger='file_load_failure', source='GETTING_USER_INPUT',
                                     dest='BAD_INPUT')
 
-        # ERROR: When the conversion fails, we go to the BAD_INPUT stage.
+        # ERROR: When the conversion fails, we go to the BAD_INPUT state.
         self.machine.add_transition(trigger='conversion_failure', source='CONVERTING_JPG_TO_JSON',
                                     dest='BAD_INPUT')
 
@@ -48,8 +46,8 @@ class PenPlotter(object):
         # The machine has finished converting the image to a JSON file.
         # Next we will need to format the JSON into a workable code.
         # TODO: Define this better.
-        self.machine.add_transition(trigger='finished_conversion', source='CONVERTING_JPG_TO_JSON', dest='FORMATTING_JSON')
-
+        self.machine.add_transition(trigger='finished_conversion', source='CONVERTING_JPG_TO_JSON',
+                                    dest='FORMATTING_JSON')
 
         # The formatting has been completed, and now it's ready, and waiting.
         self.machine.add_transition(trigger='formatting_complete', source='FORMATTING_JSON',
@@ -85,10 +83,3 @@ class PenPlotter(object):
                                     dest='IDLE')
 
         # We need to figure out the Default State.
-
-
-
-
-
-
-
