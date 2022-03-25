bd200b2d5c3cb36860876c960bbcbe25112ea26c
ncardamone10 <47370670+ncardamone10@users.noreply.github.com>
2022-03-24 20:49:38 -0400

    Allow non 8bit pwm value to work for conversion

--- BrachioGraph/angle_to_pwm.py
+++ BrachioGraph/angle_to_pwm.py
@@ -4,11 +4,13 @@
 # takes a list of lists
 # ex. [[60,75],[30,50],[120,20]]
 def angle_to_pwm(array_of_angles):
+    k = 8 # number of bits
+    pwm_resolution = 2**k - 1 # resolution of pwm
     array_of_pwm = []
     for pair in array_of_angles:
         pair_list = []
         for angle in pair:
-            pwm = (angle / 180)*255
+            pwm = (angle / 180)*pwm_resolution
             pair_list.append(pwm)
         array_of_pwm.append(pair_list)
     return array_of_pwm
