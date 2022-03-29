09f1af1e4785d2ccf522c6f00057bc8e14ea3d1d
CDelongchamp <52896126+CDelongchamp@users.noreply.github.com>
2022-03-29 13:01:10 -0400

    Fixed some formatting, added new function
    Fixed some spacing issues, and I added a new function to convert to greyscale, but we may not use it.

--- BrachioGraph/linedraw.py
+++ BrachioGraph/linedraw.py
@@ -28,30 +28,35 @@ except:
 # -------------- output functions --------------
 
 def image_to_json(
-    image_filename, resolution=1024,
-    draw_contours=False, repeat_contours=1,
-    draw_hatch=False, repeat_hatch=1,
-    ):
-
-    lines=vectorise(
+        image_filename, resolution=1024,
+        draw_contours=False, repeat_contours=1,
+        draw_hatch=False, repeat_hatch=1,
+):
+    lines = vectorise(
         image_filename, resolution,
         draw_contours, repeat_contours,
         draw_hatch, repeat_hatch,
-        )
+    )
 
     filename = json_folder + image_filename + ".json"
     lines_to_file(lines, filename)
 
 
+def convertToGreyscale(image):
+    result = image.convert('L', dither=None)
+    result = result.posterize(result, 2)
+    return result
+
+
 def makesvg(lines):
     print("generating svg file...")
-    width = math.ceil(max([max([p[0]*0.5 for p in l]) for l in lines]))
-    height = math.ceil(max([max([p[1]*0.5 for p in l]) for l in lines]))
+    width = math.ceil(max([max([p[0] * 0.5 for p in l]) for l in lines]))
+    height = math.ceil(max([max([p[1] * 0.5 for p in l]) for l in lines]))
     out = '<svg xmlns="http://www.w3.org/2000/svg" height="%spx" width="%spx" version="1.1">' % (height, width)
 
     for l in lines:
-        l = ",".join([str(p[0]*0.5)+","+str(p[1]*0.5) for p in l])
-        out += '<polyline points="'+l+'" stroke="black" stroke-width="1" fill="none" />\n'
+        l = ",".join([str(p[0] * 0.5) + "," + str(p[1] * 0.5) for p in l])
+        out += '<polyline points="' + l + '" stroke="black" stroke-width="1" fill="none" />\n'
     out += '</svg>'
     return out
 
@@ -75,10 +80,10 @@ def draw(lines):
     for line in lines:
         x, y = line[0]
         t.up()
-        t.goto(x*800/1024-400,-(y*800/1024-400))
+        t.goto(x * 800 / 1024 - 400, -(y * 800 / 1024 - 400))
         for point in line:
             t.down()
-            t.goto(point[0]*800/1024-400,-(point[1]*800/1024-400))
+            t.goto(point[0] * 800 / 1024 - 400, -(point[1] * 800 / 1024 - 400))
 
     s.mainloop()
 
@@ -86,18 +91,17 @@ def draw(lines):
 # -------------- conversion control --------------
 
 def vectorise(
-    image_filename, resolution=1024,
-    draw_contours=False, repeat_contours=1,
-    draw_hatch=False, repeat_hatch=1,
-    ):
-
+        image_filename, resolution=1024,
+        draw_contours=False, repeat_contours=1,
+        draw_hatch=False, repeat_hatch=1,
+):
     image = None
     possible = [
         image_filename,
-        "images/"+image_filename,
-        "images/"+image_filename+".jpg",
-        "images/"+image_filename+".png",
-        "images/"+image_filename+".tif"
+        "images/" + image_filename,
+        "images/" + image_filename + ".jpg",
+        "images/" + image_filename + ".png",
+        "images/" + image_filename + ".tif"
     ]
 
     for p in possible:
@@ -106,19 +110,19 @@ def vectorise(
             break
         except:
             pass
-    w,h = image.size
+    w, h = image.size
 
     # convert the image to greyscale
     image = image.convert("L")
 
     # maximise contrast
-    image=ImageOps.autocontrast(image, 10)
+    image = ImageOps.autocontrast(image, 10)
 
     lines = []
 
     if draw_contours and repeat_contours:
         contours = sortlines(getcontours(
-            image.resize((int(resolution/draw_contours), int(resolution/draw_contours*h/w))),
+            image.resize((int(resolution / draw_contours), int(resolution / draw_contours * h / w))),
             draw_contours
         ))
         for r in range(repeat_contours):
@@ -128,12 +132,11 @@ def vectorise(
         hatches = sortlines(
             hatch(
                 # image,
-                image.resize((int(resolution/draw_hatch), int(resolution/draw_hatch*h/w))),
+                image.resize((int(resolution / draw_hatch), int(resolution / draw_hatch * h / w))),
                 draw_hatch
-        ))
+            ))
         for r in range(repeat_hatch):
             lines += hatches
-
 
     f = open(svg_folder + image_filename + ".svg", 'w')
     f.write(makesvg(lines))
@@ -152,38 +155,36 @@ def getcontours(image, draw_contours=2):
     print("generating contours...")
     image = find_edges(image)
     IM1 = image.copy()
-    IM2 = image.rotate(-90,expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+    IM2 = image.rotate(-90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
     dots1 = getdots(IM1)
     contours1 = connectdots(dots1)
     dots2 = getdots(IM2)
     contours2 = connectdots(dots2)
 
     for i in range(len(contours2)):
-        contours2[i] = [(c[1],c[0]) for c in contours2[i]]
-    contours = contours1+contours2
+        contours2[i] = [(c[1], c[0]) for c in contours2[i]]
+    contours = contours1 + contours2
 
     for i in range(len(contours)):
         for j in range(len(contours)):
-            if len(contours[i]) > 0 and len(contours[j])>0:
-                if distsum(contours[j][0],contours[i][-1]) < 8:
-                    contours[i] = contours[i]+contours[j]
+            if len(contours[i]) > 0 and len(contours[j]) > 0:
+                if distsum(contours[j][0], contours[i][-1]) < 8:
+                    contours[i] = contours[i] + contours[j]
                     contours[j] = []
 
     for i in range(len(contours)):
-        contours[i] = [contours[i][j] for j in range(0,len(contours[i]),8)]
-
+        contours[i] = [contours[i][j] for j in range(0, len(contours[i]), 8)]
 
     contours = [c for c in contours if len(c) > 1]
 
-    for i in range(0,len(contours)):
-        contours[i] = [(v[0]*draw_contours,v[1]*draw_contours) for v in contours[i]]
+    for i in range(0, len(contours)):
+        contours[i] = [(v[0] * draw_contours, v[1] * draw_contours) for v in contours[i]]
 
     return contours
 
 
 # improved, faster and easier to understand hatching
 def hatch(image, draw_hatch=16):
-
     t0 = time.time()
 
     print("hatching using hatch()...")
@@ -203,19 +204,20 @@ def hatch(image, draw_hatch=16):
                 pass
 
             # above 64, draw horizontal lines
-            elif pixels[x0,y0] > 64:
-                lg1.append([(x,y+draw_hatch/4),(x+draw_hatch,y+draw_hatch/4)])
+            elif pixels[x0, y0] > 64:
+                lg1.append([(x, y + draw_hatch / 4), (x + draw_hatch, y + draw_hatch / 4)])
 
             # above 16, draw diagonal lines also
-            elif pixels[x0,y0] > 16:
-                lg1.append([(x,y+draw_hatch/4),(x+draw_hatch,y+draw_hatch/4)])
-                lg2.append([(x+draw_hatch,y),(x,y+draw_hatch)])
+            elif pixels[x0, y0] > 16:
+                lg1.append([(x, y + draw_hatch / 4), (x + draw_hatch, y + draw_hatch / 4)])
+                lg2.append([(x + draw_hatch, y), (x, y + draw_hatch)])
 
             # below 16, draw diagonal lines and a second horizontal line
             else:
-                lg1.append([(x,y+draw_hatch/4),(x+draw_hatch,y+draw_hatch/4)])  # horizontal lines
-                lg1.append([(x,y+draw_hatch/2+draw_hatch/4),(x+draw_hatch,y+draw_hatch/2+draw_hatch/4)])  # horizontal lines with additional offset
-                lg2.append([(x+draw_hatch,y),(x,y+draw_hatch)])                 # diagonal lines, left
+                lg1.append([(x, y + draw_hatch / 4), (x + draw_hatch, y + draw_hatch / 4)])  # horizontal lines
+                lg1.append([(x, y + draw_hatch / 2 + draw_hatch / 4), (
+                x + draw_hatch, y + draw_hatch / 2 + draw_hatch / 4)])  # horizontal lines with additional offset
+                lg2.append([(x + draw_hatch, y), (x, y + draw_hatch)])  # diagonal lines, left
 
     t1 = time.time()
 
@@ -258,12 +260,12 @@ def hatch(image, draw_hatch=16):
 def find_edges(image):
     print("finding edges...")
     if no_cv:
-        #appmask(IM,[F_Blur])
-        appmask(image,[F_SobelX,F_SobelY])
+        # appmask(IM,[F_Blur])
+        appmask(image, [F_SobelX, F_SobelY])
     else:
         im = np.array(image)
-        im = cv2.GaussianBlur(im,(3,3),0)
-        im = cv2.Canny(im,100,200)
+        im = cv2.GaussianBlur(im, (3, 3), 0)
+        im = cv2.Canny(im, 100, 200)
         image = Image.fromarray(im)
     return image.point(lambda p: p > 128 and 255)
 
@@ -272,18 +274,18 @@ def getdots(IM):
     print("getting contour points...")
     PX = IM.load()
     dots = []
-    w,h = IM.size
-    for y in range(h-1):
+    w, h = IM.size
+    for y in range(h - 1):
         row = []
-        for x in range(1,w):
-            if PX[x,y] == 255:
+        for x in range(1, w):
+            if PX[x, y] == 255:
                 if len(row) > 0:
-                    if x-row[-1][0] == row[-1][-1]+1:
-                        row[-1] = (row[-1][0],row[-1][-1]+1)
+                    if x - row[-1][0] == row[-1][-1] + 1:
+                        row[-1] = (row[-1][0], row[-1][-1] + 1)
                     else:
-                        row.append((x,0))
+                        row.append((x, 0))
                 else:
-                    row.append((x,0))
+                    row.append((x, 0))
         dots.append(row)
     return dots
 
@@ -292,31 +294,31 @@ def connectdots(dots):
     print("connecting contour points...")
     contours = []
     for y in range(len(dots)):
-        for x,v in dots[y]:
+        for x, v in dots[y]:
             if v > -1:
                 if y == 0:
-                    contours.append([(x,y)])
+                    contours.append([(x, y)])
                 else:
                     closest = -1
                     cdist = 100
-                    for x0,v0 in dots[y-1]:
-                        if abs(x0-x) < cdist:
-                            cdist = abs(x0-x)
+                    for x0, v0 in dots[y - 1]:
+                        if abs(x0 - x) < cdist:
+                            cdist = abs(x0 - x)
                             closest = x0
 
                     if cdist > 3:
-                        contours.append([(x,y)])
+                        contours.append([(x, y)])
                     else:
                         found = 0
                         for i in range(len(contours)):
-                            if contours[i][-1] == (closest,y-1):
-                                contours[i].append((x,y,))
+                            if contours[i][-1] == (closest, y - 1):
+                                contours[i].append((x, y,))
                                 found = 1
                                 break
                         if found == 0:
-                            contours.append([(x,y)])
+                            contours.append([(x, y)])
         for c in contours:
-            if c[-1][1] < y-1 and len(c)<4:
+            if c[-1][1] < y - 1 and len(c) < 4:
                 contours.remove(c)
     return contours
 
@@ -328,21 +330,20 @@ def sortlines(lines):
     clines = lines[:]
     slines = [clines.pop(0)]
     while clines != []:
-        x,s,r = None,1000000,False
+        x, s, r = None, 1000000, False
         for l in clines:
-            d = distsum(l[0],slines[-1][-1])
-            dr = distsum(l[-1],slines[-1][-1])
+            d = distsum(l[0], slines[-1][-1])
+            dr = distsum(l[-1], slines[-1][-1])
             if d < s:
-                x,s,r = l[:],d,False
+                x, s, r = l[:], d, False
             if dr < s:
-                x,s,r = l[:],s,True
+                x, s, r = l[:], s, True
 
         clines.remove(x)
         if r == True:
             x = x[::-1]
         slines.append(x)
     return slines
-
 
 
 def lines_to_file(lines, filename):
@@ -353,44 +354,46 @@ def lines_to_file(lines, filename):
 # -------------- helper functions --------------
 
 def midpt(*args):
-    xs,ys = 0,0
+    xs, ys = 0, 0
     for p in args:
         xs += p[0]
         ys += p[1]
-    return xs/len(args),ys/len(args)
+    return xs / len(args), ys / len(args)
 
 
 def distsum(*args):
-    return sum([ ((args[i][0]-args[i-1][0])**2 + (args[i][1]-args[i-1][1])**2)**0.5 for i in range(1,len(args))])
+    return sum(
+        [((args[i][0] - args[i - 1][0]) ** 2 + (args[i][1] - args[i - 1][1]) ** 2) ** 0.5 for i in range(1, len(args))])
 
 
 # -------------- code used when open CV is not available  --------------
 
 
-def appmask(IM,masks):
+def appmask(IM, masks):
     PX = IM.load()
-    w,h = IM.size
+    w, h = IM.size
     NPX = {}
-    for x in range(0,w):
-        for y in range(0,h):
-            a = [0]*len(masks)
+    for x in range(0, w):
+        for y in range(0, h):
+            a = [0] * len(masks)
             for i in range(len(masks)):
                 for p in masks[i].keys():
-                    if 0<x+p[0]<w and 0<y+p[1]<h:
-                        a[i] += PX[x+p[0],y+p[1]] * masks[i][p]
-                if sum(masks[i].values())!=0:
+                    if 0 < x + p[0] < w and 0 < y + p[1] < h:
+                        a[i] += PX[x + p[0], y + p[1]] * masks[i][p]
+                if sum(masks[i].values()) != 0:
                     a[i] = a[i] / sum(masks[i].values())
-            NPX[x,y]=int(sum([v**2 for v in a])**0.5)
-    for x in range(0,w):
-        for y in range(0,h):
-            PX[x,y] = NPX[x,y]
+            NPX[x, y] = int(sum([v ** 2 for v in a]) ** 0.5)
+    for x in range(0, w):
+        for y in range(0, h):
+            PX[x, y] = NPX[x, y]
+
 
 F_Blur = {
-    (-2,-2):2,(-1,-2):4,(0,-2):5,(1,-2):4,(2,-2):2,
-    (-2,-1):4,(-1,-1):9,(0,-1):12,(1,-1):9,(2,-1):4,
-    (-2,0):5,(-1,0):12,(0,0):15,(1,0):12,(2,0):5,
-    (-2,1):4,(-1,1):9,(0,1):12,(1,1):9,(2,1):4,
-    (-2,2):2,(-1,2):4,(0,2):5,(1,2):4,(2,2):2,
+    (-2, -2): 2, (-1, -2): 4, (0, -2): 5, (1, -2): 4, (2, -2): 2,
+    (-2, -1): 4, (-1, -1): 9, (0, -1): 12, (1, -1): 9, (2, -1): 4,
+    (-2, 0): 5, (-1, 0): 12, (0, 0): 15, (1, 0): 12, (2, 0): 5,
+    (-2, 1): 4, (-1, 1): 9, (0, 1): 12, (1, 1): 9, (2, 1): 4,
+    (-2, 2): 2, (-1, 2): 4, (0, 2): 5, (1, 2): 4, (2, 2): 2,
 }
-F_SobelX = {(-1,-1):1,(0,-1):0,(1,-1):-1,(-1,0):2,(0,0):0,(1,0):-2,(-1,1):1,(0,1):0,(1,1):-1}
-F_SobelY = {(-1,-1):1,(0,-1):2,(1,-1):1,(-1,0):0,(0,0):0,(1,0):0,(-1,1):-1,(0,1):-2,(1,1):-1}
+F_SobelX = {(-1, -1): 1, (0, -1): 0, (1, -1): -1, (-1, 0): 2, (0, 0): 0, (1, 0): -2, (-1, 1): 1, (0, 1): 0, (1, 1): -1}
+F_SobelY = {(-1, -1): 1, (0, -1): 2, (1, -1): 1, (-1, 0): 0, (0, 0): 0, (1, 0): 0, (-1, 1): -1, (0, 1): -2, (1, 1): -1}
--- BrachioGraph/main.py
+++ BrachioGraph/main.py
@@ -11,7 +11,7 @@ bicep = 10.0
 # gotta figure out if this is actually origin
 origin = [0,0]
 
-image_to_json("deathly_hallows.jpg", draw_contours=2, draw_hatch=0)
+image_to_json("Cute_Dog.png", draw_contours=2, draw_hatch=0)
 #image_to_json("africa.jpg", draw_contours=2, draw_hatch=0)
 #This code finds an image with the indicated name in the 'images' directory and:
 #    -draws the contours and hatch lines
@@ -23,7 +23,7 @@ image_to_json("deathly_hallows.jpg", draw_contours=2, draw_hatch=0)
 #    draw_hatch = 16 (between 8 and 16)
 
 #json_data = json_reader.read_json("images/africa.jpg.json")
-json_data = json_reader.read_json("images/deathly_hallows.jpg.json")
+json_data = json_reader.read_json("images/Cute_Dog.jpg.json")
 #array_transformed = transform_array(json_data)
 #json_reader.write_to_text(json_data)
 #json_reader.write_to_json(json_data)
