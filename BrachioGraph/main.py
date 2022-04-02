801549a7a25778a73db53de831ed54f1a5973087
Olivia Borel <oliviaborel@gmail.com>
2022-04-02 16:00:34 -0400

    initiate the brachiograph, process and draw the image with one command

--- BrachioGraph/main.py
+++ BrachioGraph/main.py
@@ -9,13 +9,12 @@ import inverse_kinematics as ik
 # Lengths of arms in cm (for now)
 forearm = 10.4
 bicep = 10.0
-# gotta figure out if this is actually origin
 origin = [0,0]
 
-#This function processes the inputted image into a JSON, a SVG, and a txt
+#This function processes the inputted image into a JSON and a SVG
 def process_image(processing_image):
     image_to_json(processing_image, draw_contours=2, draw_hatch=16)
-    #image_to_json("africa.jpg", draw_contours=2, draw_hatch=0)
+
     #This code finds an image with the indicated name in the 'images' directory and:
     #    -draws the contours and hatch lines
     #    -creates a JSON file with the same name
@@ -25,22 +24,22 @@ def process_image(processing_image):
     #    countours = 2 (between 0.5 and 4)
     #    draw_hatch = 16 (between 8 and 16)
 
-    #json_data = json_reader.read_json("images/africa.jpg.json")
     json_data = json_reader.read_json("images/" + processing_image + ".json")
-    #array_transformed = transform_array(json_data)
-    #json_reader.write_to_text(json_data)
-    #json_reader.write_to_json(json_data)
-
-    #cwd = os.getcwd()
-    #os.chdir(cwd + "/images")
-    #cwd = os.getcwd()
-    #exec(open('json_reader.py').read())
 
 
 def draw_image(drawing_image):
     bg = BrachioGraph()
     bg.plot_file(drawing_image)
 
-drawing_image = sys.argv[0]
+##############################################################################
+# run this file name and include the name of the image as a system argument
+# ex. "python main.py X.jpg"
+# X.jpg is considered system argument 1
+##############################################################################
+drawing_image = sys.argv[1]
+
+# processing the image using image_to_json()
 process_image(drawing_image)
+
+# drawing the image using plot_file()
 draw_image(drawing_image)
